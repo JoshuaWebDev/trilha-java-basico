7869c0819f8d8cad0d91131cad6b61d4129bc38d
Josué Barros da Silva <josue.barros1986@gmail.com>
2024-04-27 18:30:34 -0300

    ternary operator

--- variables-types/src/Operators.java
+++ variables-types/src/Operators.java
@@ -1,5 +1,16 @@
-public class Operadores {
+public class Operators {
     public static void main(String[] args) {
+        int a, b, c;
+        a = 5;
+        b = 6;
+        c = 6;
+
+        String result = a == b ? "a is equal b" : "a is not equal b";
+        System.out.println(result);
+
+        result = b == c ? "b is equal c" : "b is not equal c";
+        System.out.println(result);
+
         String concatenation = "?";
 
         concatenation = 1 + 1 + 1 + "1";
