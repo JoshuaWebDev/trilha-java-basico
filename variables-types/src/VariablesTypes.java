8400695f801a74d314bbf7310cd0e86f52ebdc6e
Josué Barros da Silva <josue.barros1986@gmail.com>
2024-04-27 16:53:37 -0300

    variables types

---
+++ variables-types/src/VariablesTypes.java
@@ -0,0 +1,12 @@
+public class VariablesTypes {
+    public static void main(String[] args) throws Exception {
+        // variables types examples
+        double salary = 2500.33;
+        short shortNumber = 1;
+        int normalNumber = shortNumber;
+        short shortNumber2 = (short) normalNumber;
+
+        // constants examples
+        final double VALOR_DE_PI = 3.14;
+    }
+}
