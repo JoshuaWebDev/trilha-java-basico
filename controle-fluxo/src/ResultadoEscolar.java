8fc0f50a50822db7115c99ca06ddabb53dbb3765
Josué Barros da Silva <josue.barros1986@gmail.com>
2024-04-29 07:22:50 -0300

    operador ternário

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,14 +1,8 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 10;
+        int nota = 7;
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
 
-        if (nota >= 7)
-            System.out.println("Aprovado");
-
-        else if (nota >= 5 && nota < 7)
-            System.out.println("Prova Recuperação");
-
-        else
-            System.out.println("Reprovado");
+        System.out.println(resultado);
     }
 }
