774d01dd622acd1c502385c9cb41f2f754537b6f
Josué Barros da Silva <josue.barros1986@gmail.com>
2024-04-30 22:42:40 -0300

    hierarquia das exceções e exceções customizadas

---
+++ controle-fluxo/src/FormatadorCepExemplo.java
@@ -0,0 +1,19 @@
+import Exceptions.CepInvalidoException;
+
+public class FormatadorCepExemplo {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("237650640");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            System.out.println("O CEP não corresponde com as regras de negócio");
+        }
+    }
+
+    static String formatarCep(String cep) throws CepInvalidoException {
+        if (cep.length() != 8)
+            throw new CepInvalidoException();
+
+        return "23.765-064";
+    }
+}
